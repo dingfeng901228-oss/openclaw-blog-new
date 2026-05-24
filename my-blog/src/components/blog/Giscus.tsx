61a508af4cac5ebd446f9e6e4a647149be564287
Frank Ding <frank@frankbot.org>
2026-05-24 12:54:46 +0900

    Fix giscus: use 'specific' mapping so data-term is used for cross-lang comment sharing

--- my-blog/src/components/blog/Giscus.tsx
+++ my-blog/src/components/blog/Giscus.tsx
@@ -43,7 +43,7 @@ export default function Giscus({
     script.setAttribute('data-repo-id', repoId)
     script.setAttribute('data-category', category)
     script.setAttribute('data-category-id', categoryId)
-    script.setAttribute('data-mapping', 'pathname')
+    script.setAttribute('data-mapping', 'specific')
     script.setAttribute('data-strict', '0')
     script.setAttribute('data-reactions-enabled', reactionsEnabled)
     script.setAttribute('data-emit-metadata', emitMetadata)
